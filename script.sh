b19d1014b4197ca9d4a1f009ec288953b4d0e584
Nguyễn Văn Đỏ <nguyenvando510@gmail.com>
2024-09-22 09:41:43 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,24 +1,6 @@
-set -e
-
-echo ">> pull code"
-git pull origin master
-
 chmod +x script.sh
-
-echo ">> build docker image"
-docker build -t nodetest:v1 . 
-
-echo ">> create pre version"
-docker commit nodetest nodetest:pre
-
-echo ">> stop current image"
-docker stop nodetest
-
-echo ">> delete current image"
-docker rm nodetest
-
-docker run --net host -d  --name nodetest nodetest:v1
-
+git pull origin master
+docker compose down
+docker compose build
+docker compose up -d
 docker ps
-
-docker logs nodetest
