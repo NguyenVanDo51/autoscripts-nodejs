5e36ecb0e1b4917cdc39d6e28ded91da34ab7a95
Nguyễn Văn Đỏ <nguyenvando510@gmail.com>
2024-09-22 10:30:06 +0700

    a

--- server.js
+++ server.js
@@ -6,11 +6,10 @@ const app = express()
 app.use(bodyParser.json())
 
 // Cấu hình view engine
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs')
 
 // Cấu hình thư mục chứa các template
-app.set('views', __dirname + '/views'); // Thay đổi nếu thư mục views của bạn khác
-
+app.set('views', __dirname + '/views') // Thay đổi nếu thư mục views của bạn khác
 
 function convertProxyFormat(proxy) {
   try {
@@ -113,7 +112,7 @@ app.put('/users', (req, res) => {
         .map((col) => `${col} = ?`)
         .join(', ')} WHERE username = ?`
       const values = [...Object.values(otherFields), username]
-        console.log('updateQuery', updateQuery)
+      console.log('updateQuery', updateQuery)
       db.run(updateQuery, values, function (err) {
         if (err) {
           return res.status(400).json({ error: err.message })
@@ -143,29 +142,53 @@ app.delete('/users/:username', (req, res) => {
 
 // Hàm trả về danh sách user
 app.get('/ui/users', (req, res) => {
-  const selectQuery = `SELECT * FROM user`;
+  const selectQuery = `SELECT * FROM user`
   db.all(selectQuery, [], (err, rows) => {
     if (err) {
-      return res.status(500).json({ error: err.message });
+      return res.status(500).json({ error: err.message })
     }
-    res.render('users', { users: rows }); // Giả sử bạn sử dụng EJS hoặc một template engine khác
-  });
-});
+    const proxyList = [
+      '14.225.66.94:12344:VN78470:sYVCmr08',
+      '180.214.239.207:2023:Proxyviet51447:iVtD3m1B',
+      '14.225.63.213:6666:rqyc8w2w:rQYC8w2W',
+      '14.225.64.181:6666:ewme5n7i:eWmE5n7I',
+      '14.225.52.232:12345:ifje4u1c:iFJE4u1c',
+      '36.50.53.131:49068:qqqn1t0f:qQqN1t0f',
+      '49.236.211.161:3179:modsbe1o:CjHWzgw34QRJ',
+      '14.225.57.7:12345:PVN77433:yVQB7z2q',
+      'prxmik1.ddns.net:20009:Acbd1234:Acbd1234^%',
+      '14.225.57.205:12345:surn0t9s:zFIK6g8d',
+    ]
+    res.render('users', { users: rows, proxyList }) // Giả sử bạn sử dụng EJS hoặc một template engine khác
+  })
+})
 // Hàm trả về form sửa user
 app.get('/ui/users/edit/:username', (req, res) => {
-  const { username } = req.params;
-  const selectQuery = `SELECT * FROM user WHERE username = ?`;
-  
+  const { username } = req.params
+  const selectQuery = `SELECT * FROM user WHERE username = ?`
+
   db.get(selectQuery, [username], (err, row) => {
     if (err) {
-      return res.status(500).json({ error: err.message });
+      return res.status(500).json({ error: err.message })
     }
     if (!row) {
-      return res.status(404).send('User not found');
+      return res.status(404).send('User not found')
     }
-    res.render('editUser', { user: row }); // Giả sử bạn có một template cho việc sửa user
-  });
-});
+    const proxyList = [
+      '14.225.66.94:12344:VN78470:sYVCmr08',
+      '180.214.239.207:2023:Proxyviet51447:iVtD3m1B',
+      '14.225.63.213:6666:rqyc8w2w:rQYC8w2W',
+      '14.225.64.181:6666:ewme5n7i:eWmE5n7I',
+      '14.225.52.232:12345:ifje4u1c:iFJE4u1c',
+      '36.50.53.131:49068:qqqn1t0f:qQqN1t0f',
+      '49.236.211.161:3179:modsbe1o:CjHWzgw34QRJ',
+      '14.225.57.7:12345:PVN77433:yVQB7z2q',
+      'prxmik1.ddns.net:20009:Acbd1234:Acbd1234^%',
+      '14.225.57.205:12345:surn0t9s:zFIK6g8d',
+    ]
+    res.render('editUser', { user: row, proxyList }) // Giả sử bạn có một template cho việc sửa user
+  })
+})
 
 // Start server
 const PORT = process.env.PORT || 3456
