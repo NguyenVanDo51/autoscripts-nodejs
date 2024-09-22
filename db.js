39b0da87904c79ff23bf84319d87efd1fba617e2
Nguyễn Văn Đỏ <nguyenvando510@gmail.com>
2024-09-22 11:10:40 +0700

    a

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const sqlite3 = require('sqlite3').verbose();
 
 // Mở kết nối đến cơ sở dữ liệu SQLite
-const db = new sqlite3.Database('./users.db', (err) => {
+const db = new sqlite3.Database('./data/users.db', (err) => {
   if (err) {
     console.error('Error opening database:', err);
   } else {
